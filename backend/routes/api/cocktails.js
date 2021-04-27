3ff9c8507eb492cd819cbbc463c29528ac6701fd
James Sullivan <jsullivan86@gmail.com>
2021-04-27 16:16:54 -0700

    demo user button is working

--- backend/routes/api/cocktails.js
+++ backend/routes/api/cocktails.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
 const { Cocktail } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth');
 const router = express.Router();
 
-router.get('/', asyncHandler(async function (req, res) {
+router.get('/', requireAuth, asyncHandler(async function (req, res) {
     const cocktails = await Cocktail.findAll();
     // console.log(cocktails)
     return res.json(cocktails);
