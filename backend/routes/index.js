69564b4ad351f80208a2ddfda5937fea209eabf1
James Sullivan <jsullivan86@gmail.com>
2021-04-30 13:12:58 -0700

    trying to deploy to heroku

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -23,7 +23,8 @@ if (process.env.NODE_ENV === 'production') {
     // Serve the frontend's index.html file at all other routes NOT starting with /api
     router.get(/^(?!\/?api).*/, (req, res) => {
         res.cookie('XSRF-TOKEN', req.csrfToken());
-        return res.sendFile(
+        // removed return from line below
+        res.sendFile(
             path.resolve(__dirname, '../../frontend', 'build', 'index.html')
         );
     });
@@ -32,7 +33,8 @@ if (process.env.NODE_ENV === 'production') {
 if (process.env.NODE_ENV !== 'production') {
     router.get('/api/csrf/restore', (req, res) => {
         res.cookie('XSRF-TOKEN', req.csrfToken());
-        return res.json({});
+        // return res.json({});
+        res.status(201).json({});
     });
 }
 module.exports = router;
